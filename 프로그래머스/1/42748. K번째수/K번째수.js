c5a383275f1aeac67af9e7447a152b9482091ff2
hyeonjeong <jeooooong22@gmail.com>
2025-04-01 15:44:22 +0900

    [level 1] Title: K번째수, Time: 0.11 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.js
+++ 프로그래머스/1/42748. K번째수/K번째수.js
@@ -1,11 +1,18 @@
-function solution(array, commands) {
-    var answer = [];
+// function solution(array, commands) {
+//     var answer = [];
     
-    commands.forEach((el) => {        
-        let slicedArr = array.slice(el[0] - 1, el[1]);
-        let sortedArr = slicedArr.sort((a, b) => a - b);
+//     commands.forEach((el) => {        
+//         let slicedArr = array.slice(el[0] - 1, el[1]);
+//         let sortedArr = slicedArr.sort((a, b) => a - b);
         
-        answer.push(sortedArr[el[2] - 1]);        
+//         answer.push(sortedArr[el[2] - 1]);        
+//     })
+//     return answer;
+// }
+
+function solution(array, commands) {
+    return commands.map((el)=> {
+        const [start, end, target] = el;
+         return array.slice(start - 1, end).sort((a,b) => a - b)[target -1]; 
     })
-    return answer;
 }
