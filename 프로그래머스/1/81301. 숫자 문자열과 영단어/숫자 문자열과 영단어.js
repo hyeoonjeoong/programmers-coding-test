a159fe9305b4187d653a446a6e28d627617916e0
hyeonjeong <jeooooong22@gmail.com>
2025-04-03 11:40:41 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.19 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -0,0 +1,17 @@
+function solution(s) {
+    var answer = 0;
+
+    const numWord = {
+        "zero": "0", "one": "1", "two": "2", "three": "3",
+        "four": "4", "five": "5", "six": "6", "seven": "7",
+        "eight": "8", "nine": "9"
+    };
+    
+    Object.keys(numWord).forEach((str)=> {
+       s = s.replaceAll(str, numWord[str])
+    })
+    
+    return Number(s);
+}
+
+    
