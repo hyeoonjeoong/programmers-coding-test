290e29fbf45b6ebae2e75f2c6975cb1a4a7cd376
hyeonjeong <jeooooong22@gmail.com>
2025-05-15 15:07:59 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 96 ms, Memory: 9496 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim().split(' ');
+
+const N = parseInt(input);
+
+for (let i = 1; i <= N; i++) {
+  console.log('*'.repeat(i));
+}
