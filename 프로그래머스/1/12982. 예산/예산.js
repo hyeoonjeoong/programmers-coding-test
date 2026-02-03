295d0ca488be3ef45642948918630cef24e2fdd1
hyeonjeong <jeooooong22@gmail.com>
2026-02-03 17:46:53 +0900

    [level 1] Title: 예산, Time: 0.22 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12982. 예산/예산.js
+++ 프로그래머스/1/12982. 예산/예산.js
@@ -1,12 +1,12 @@
 function solution(d, budget) {
-    let sortedCosts = d.sort((a,b)=> a-b);
-    let totalPrice =0;
+    const sorted = [...d].sort((a, b) => a - b);
     let count = 0;
-
-    for(let i=0; i<sortedCosts.length; i++){
-        totalPrice += sortedCosts[i];
-        if(totalPrice > budget) break;
-        count++;
+    let remain = budget;
+    
+    for(const cost of sorted){
+        if(remain < cost) break;
+        remain -= cost;
+        count ++;
     }
     return count;
 }
