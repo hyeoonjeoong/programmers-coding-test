0268c786d9f50b9b0cdf3d8a259f132d34cf5e3c
hyeonjeong <jeooooong22@gmail.com>
2026-04-03 17:52:38 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 57.87 ms, Memory: 70.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -0,0 +1,17 @@
+function solution(participant, completion) {
+    const obj = {};
+    
+    participant.forEach((el) => {
+        obj[el] = (obj[el] || 0) + 1;
+    })
+    
+    completion.forEach((el) => {
+        obj[el] = obj[el] - 1;
+    })
+    
+    for (let name in obj){
+        if(obj[name] === 1){
+            return name;
+        }
+    }
+}
